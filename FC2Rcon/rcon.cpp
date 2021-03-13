cbca66343b660b7387685ef7e2137559ab717ee9
Halvor Lyche Strandvoll <halvors@halvors.org>
2021-03-13 13:39:05 +0100

    More work.

--- FC2Rcon/rcon.cpp
+++ FC2Rcon/rcon.cpp
@@ -3,42 +3,62 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+#include <QDir>
+#include <QProcess>
+
 rcon::rcon(QObject *parent) : QObject(parent)
 {
+    const QString workDir = "/mnt/applications/SteamLibrary/steamapps/common/Far Cry 2/bin";
+    const QString execFile = "FC2ServerLauncher.exe";
+
     proc = new QProcess(this);
-    proc->setWorkingDirectory("/mnt/applications/SteamLibrary/steamapps/common/Far Cry 2/bin");
+    proc->setWorkingDirectory(workDir);
     proc->setProgram("wine");
-    proc->setArguments(QStringList() << "FC2ServerLauncher.exe" << "-borderless" << "-noredirectstdin");
+    proc->setArguments(QStringList() << execFile << "-noredirectstdin");
     proc->setProcessChannelMode(QProcess::SeparateChannels);
-    proc->setReadChannel(QProcess::ProcessChannel::StandardOutput);
+
+    proc->setCreateProcessArgumentsModifier([] (QProcess::CreateProcessArguments *args)
+        {
+            args->flags |= CREATE_NEW_CONSOLE;
+            args->startupInfo->dwFlags &= ~STARTF_USESTDHANDLES;
+            args->startupInfo->dwFlags |= STARTF_USEFILLATTRIBUTE;
+            args->startupInfo->dwFillAttribute = BACKGROUND_BLUE | FOREGROUND_RED
+                                               | FOREGROUND_INTENSITY;
+        });
 
     connect(proc, &QProcess::readyReadStandardOutput, this, &rcon::readyReadStandardOutput);
     connect(proc, &QProcess::readyReadStandardError, this, &rcon::readyReadStandardError);
     connect(proc, &QProcess::readyRead, this, &rcon::readyRead);
 
+    //QString qsExePath = "C:\\Program Files (x86)\\Some Company\\SomeApp.exe";
+    bool bStarted = proc->startDetached("wine /mnt/applications/SteamLibrary/steamapps/common/Far Cry 2/bin/FC2ServerLauncher.exe", QStringList());
+
     // Start the process
-    proc->start();
-    QCoreApplication::processEvents();
+    //proc->start();
+    //QCoreApplication::processEvents();
     proc->waitForStarted();
+
+    qDebug() << "Process started";
 
     // Read the output
     proc->waitForReadyRead();
-    //QString output(proc->readAllStandardOutput());
+
+    qDebug() << "Ready to write";
 
     // Write the data
-    sendCommand("net_GetCurrentMapName");            // Displays the current map name.
-    sendCommand("net_GetCurrentGameModeName");     // Displays the current game mode name.
+    //sendCommand("net_GetCurrentMapName");          // Displays the current map name.
+    //sendCommand("net_GetCurrentGameModeName");     // Displays the current game mode name.
     sendCommand("net_GetHostAddress");             // Displays the host address.
-    sendCommand("net_GetHostName");                // Displays the host name.
-    sendCommand("net_GetCurrentSessionMaxPlayer"); // Displays the maximum number of players for the current session.
-    sendCommand("net_GetPlayerList");              // Get the player list.
-    sendCommand("net_GetPlayerListByTeam");        // Get the player list by team.
-    sendCommand("net_GetCaptainNames");            // Get the name of each captain.
-    sendCommand("net_GetGameScoreStats");          // Get the current game score statistics.
+    //sendCommand("net_GetHostName");                // Displays the host name.
+    //sendCommand("net_GetCurrentSessionMaxPlayer"); // Displays the maximum number of players for the current session.
+    //sendCommand("net_GetPlayerList");              // Get the player list.
+    //sendCommand("net_GetPlayerListByTeam");        // Get the player list by team.
+    //sendCommand("net_GetCaptainNames");            // Get the name of each captain.
+    //sendCommand("net_GetGameScoreStats");          // Get the current game score statistics.
     //proc->closeWriteChannel();
 
-    proc->waitForReadyRead();
-    qDebug() << proc->readAll();
+    //proc->waitForReadyRead();
+    //qDebug() << proc->readAll();
 
     // Wait for finished
     proc->waitForFinished();
@@ -53,12 +73,13 @@ void rcon::sendCommand(const QString &cmd)
 void rcon::readyReadStandardOutput()
 {
     qDebug() << "Got stdout!";
+    qDebug() << proc->readAllStandardOutput();
 }
 
 void rcon::readyReadStandardError()
 {
-    //qDebug() << "Got stderr!";
-    //qDebug() << proc->readAllStandardError();
+    qDebug() << "Got stderr!";
+    qDebug() << proc->readAllStandardError();
 }
 
 void rcon::readyRead()
